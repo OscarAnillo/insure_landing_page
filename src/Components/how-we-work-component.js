520f23168e1b23dc339ff5d9ad91bcd22e338312
oscar anillo <oscarani14@gmail.com>
2022-11-07 22:14:59 -0500

    add how we work component flex

--- src/Components/how-we-work-component.js
+++ src/Components/how-we-work-component.js
@@ -5,7 +5,7 @@ export default function HowWeWorkComponent(){
                 <h1>Find out more about how we work</h1>
                 <button>How we work</button>
             </div>
-            <div className="we-work-bg"></div>
+            <div className="we-work-bg we-work-bg-desktop"></div>
         </div>
     )
 }
