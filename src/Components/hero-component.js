7c3069ea30ba6585b2b166564e889972c5a2dae5
oscar anillo <oscarani14@gmail.com>
2022-11-07 20:24:21 -0500

    add desktop view nav and hero view flex

--- src/Components/hero-component.js
+++ src/Components/hero-component.js
@@ -3,7 +3,8 @@ export default function HeroComponent(){
         <div className="hero-bg">
             <div className="hero-container">
                 <div>
-                    <img src="images/image-intro-mobile.jpg" alt="" />
+                    <img src="images/image-intro-mobile.jpg" alt="" className="mobile-img-intro" />
+                    <img src="images/image-intro-desktop.jpg" alt="" className="desktop-img-intro" />
                 </div>
                 <div>
                     <h1>Humanizing your insurance.</h1>
@@ -11,8 +12,8 @@ export default function HeroComponent(){
                     <button>View plans</button>
                 </div>
             </div>
-            <div className="mobile-left-intro"></div>
-            <div className="mobile-right-intro"></div>
+            <div className="mobile-left-intro desktop-left-intro"></div>
+            <div className="mobile-right-intro desktop-right-intro"></div>
         </div>
     )
 }
